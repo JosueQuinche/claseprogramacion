84eadbf03553db317f8bf018032b01d224e08e21
unknown <quinchej659@gmail.com>
2022-10-25 11:30:50 -0500

    chiquitrosfs

---
+++ src/Contenido.java
@@ -0,0 +1,25 @@
+public class Contenido {
+    public static void main(String[] args) {
+        System.out.println("20 conceptos basicos de programacion");
+        System.out.println("Algoritmo");
+        System.out.println("Lenguaje de programacion");
+        System.out.println("Programa fuente");
+        System.out.println("Programa objeto");
+        System.out.println("Compilador");
+        System.out.println("Pseudocodigo");
+        System.out.println("Daigrama de flujo");
+        System.out.println("Programacion imperativa");
+        System.out.println("Programacion etructurada");
+        System.out.println("Programacion logica");
+        System.out.println("Programacion declarativa ");
+        System.out.println("Programacion orienteada a objetos");
+        System.out.println("Programacion dirigida a eventos");
+        System.out.println("Compilacion-Depuracion");
+        System.out.println("Identificadores");
+        System.out.println("Variables-Constantes");
+        System.out.println("Datos");
+        System.out.println("Operadores");
+        System.out.println("Estrucutras de Control");
+        System.out.println("Mantenimiento");
+    }
+}
